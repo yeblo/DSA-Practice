36e3ce9af45628ca7ab157d5a9d24027a21cea73
yeblo <yeblo95@yahoo.com>
2022-10-27 01:28:59 -0500

    more linkedlist problems

--- LinkedList/addTwoList.js
+++ LinkedList/addTwoList.js
@@ -46,3 +46,40 @@ var addTwoNumbers = function(l1, l2) {
 return resNode.next;
 
 };
+
+
+
+var addTwoNumbers = function(l1, l2) {
+    var sum = 0;
+
+    //first init
+    var resNode = new ListNode();
+    var copyNode = resNode;
+    
+    //start iteration
+    while(l1!=undefined || l2!=undefined || sum!= 0)
+    {
+        if(l1!=undefined)
+        {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        
+        if(l2!=undefined)
+        {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        
+        var newNode = new ListNode();
+        newNode.val = sum%10;
+        copyNode.next = newNode;
+        copyNode = newNode;
+        
+        sum = parseInt(sum/10);
+        
+    }
+    
+return resNode.next;
+
+};
--- LinkedList/palindromeLinkedList.js
+++ LinkedList/palindromeLinkedList.js
@@ -1,4 +1,43 @@
 
+
+//using fast pointer slow pointer approach
+const isPalindrome1 = function(head) {
+    //Approach
+        //Fast Pointer, Slow Pointer
+        //once slow is in the middle, reverse half of the list
+        //compare it to the current list
+        let fast = head
+        let slow = head
+        let next = null;
+        let prev = null
+        while(fast !== null && fast.next !== null){
+            fast = fast.next.next
+            slow = slow.next 
+        }
+
+    //reversed list
+        while(slow != null){
+            next = slow.next;
+            slow.next = prev;
+            prev = slow
+            slow = next
+        }
+  
+  while(prev !== null){
+      if(prev.val != head.val){
+          return false
+      }
+
+      prev = prev.next;
+      head = head.next;
+  }
+return true
+
+    
+};
+
+
+
 //using arrays
 const isPalindrome = function (head){
     let array = []

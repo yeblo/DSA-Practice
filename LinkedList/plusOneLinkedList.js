dda12fa41a1f16635ea6ab57cf43461a911d8d52
yeblo <yeblo95@yahoo.com>
2022-10-29 01:38:00 -0500

    Update plusOneLinkedList.js

--- LinkedList/plusOneLinkedList.js
+++ LinkedList/plusOneLinkedList.js
@@ -1,30 +1,109 @@
-var plusOne = function(head) {
-    let i = 0;
-     let storNum = ''
-     let curr = head
-     storNum += `${curr.val}`
+const plusOne = function(head) {
+    //reverse list
+     let reverse = reverseList(head)
      
-     while(curr.next != null)
-     {
+     let curr = reverse
+     let previous = reverse
+
+     let sum = curr.val + 1
+     let carry =  parseInt(sum / 10)
+     curr.val = sum % 10
+     curr = curr.next
+
+     while(curr != null){
+         previous = curr
+         sum = curr.val + carry
+         curr.val = sum % 10
+         carry = parseInt(sum/10)
          curr = curr.next
-         storNum += `${curr.val}`
-         i++;
      }
+
+     if(carry > 0){
+         let carryNode = new ListNode(carry)
+         previous.next = carryNode
+     }
+  
+
+    return reverseList(reverse)
+    
+};
+
+
+const reverseList = function(head){
+    let next = null
+    let prev = null
+    let curr = head
+
+    while(curr != null){
+        next = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next
+    }
+
+    return prev
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var plusOne = function(head) {
+//     let i = 0;
+//      let storNum = ''
+//      let curr = head
+//      storNum += `${curr.val}`
      
-     let sum = Number(storNum) + 1
-     console.log(sum)
+//      while(curr.next != null)
+//      {
+//          curr = curr.next
+//          storNum += `${curr.val}`
+//          i++;
+//      }
+     
+//      let sum = Number(storNum) + 1
+//      console.log(sum)
  
-     let dummyNode = new ListNode(0)
-     let copyNode = dummyNode
+//      let dummyNode = new ListNode(0)
+//      let copyNode = dummyNode
  
-     let newSum = sum.toString()
-     for(let j = 0; j< newSum.length; j++)
-     {
-         let newNode = new ListNode()
-         newNode.val = newSum.charAt(j)
-         copyNode.next = newNode
-         copyNode = copyNode.next
-     }    
+//      let newSum = sum.toString()
+//      for(let j = 0; j< newSum.length; j++)
+//      {
+//          let newNode = new ListNode()
+//          newNode.val = newSum.charAt(j)
+//          copyNode.next = newNode
+//          copyNode = copyNode.next
+//      }    
  
-     return dummyNode.next
- };
+//      return dummyNode.next
+//  };
